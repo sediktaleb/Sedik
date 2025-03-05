8269012100d17710757b711f473c07928b7fe0dc
sediktaleb <167780734+sediktaleb@users.noreply.github.com>
2025-03-05 14:02:33 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,54 +4,58 @@ document.getElementById("loader1").style.display="none";
 function Send(){
 	var Person = document.getElementById("Names").value;
 	var Message = document.getElementById("Message").value;
-	document.getElementById("loader").style.display="inline";
-	alert("hello");
-	if(Person == "Sedik"){
-		sendEmail("sedikalgriadz@gmail.com",Message)
+	if(Message){
+		document.getElementById("loader").style.display="inline";
+		if(Person == "Sedik"){
+			sendEmail("sedikalgriadz@gmail.com",Message)
+		}
+		if(Person == "Sirine"){
+			sendEmail("ararsirine2007@gmail.com",Message)
 
-	}
-	if(Person == "Sirine"){
-		sendEmail("ararsirine2007@gmail.com",Message)
+		}
+		if(Person == "Charaf"){
+			sendEmail("bousbaamin8@gmail.com",Message)
 
-	}
-	if(Person == "Charaf"){
-		sendEmail("bousbaamin8@gmail.com",Message)
+		}
+		if(Person == "Fwaz"){
+			sendEmail("zdiclassg@gmail.com",Message)
 
-	}
-	if(Person == "Fwaz"){
-		sendEmail("zdiclassg@gmail.com",Message)
-
-	}
-	if(Person == "AbdJalil"){
-		sendEmail("sajalil217@gmail.com",Message)
+		}
+		if(Person == "AbdJalil"){
+			sendEmail("sajalil217@gmail.com",Message)
 
 
-	}
-	if(Person == "Iyad"){
-		sendEmail("khemissigracha@gmail.com",Message)
-	
-	}
-	if(Person == "Aryam"){
-		alert("لم يتم التحصل على الايميل")
-	}
-	if(Person == "Douaa"){
-		sendEmail("Dodosinyorita3@gmail.com",Message)
-	}
-	if(Person == "Malak"){
-		sendEmail("dk.karima.2020@gmail.com",Message)
+		}
+		if(Person == "Iyad"){
+			sendEmail("khemissigracha@gmail.com",Message)
 		
+		}
+		if(Person == "Aryam"){
+			alert("لم يتم التحصل على الايميل")
+		}
+		if(Person == "Douaa"){
+			sendEmail("Dodosinyorita3@gmail.com",Message)
+		}
+		if(Person == "Malak"){
+			sendEmail("dk.karima.2020@gmail.com",Message)
+			
+		}
+	}else{
+		alert("اكتب رسالة");
 	}
 }
 
-function sendEmail(email,meesage){
-    var parmas = {
-        message : meesage,
-        Email : email,
+function sendEmail(email,message){
+	alert(message)
+	var parmas = {
+        message : message,
+        email : email,
 
 
     };
     const serviceID = "service_fsioy4u";
     const templateID = "template_ppseq1b";
+
     emailjs.send(serviceID,templateID,parmas).then(function(red){
 
         alert("الرسالة وصـلت");
